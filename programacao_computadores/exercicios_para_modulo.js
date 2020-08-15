0c47115273a1d343ffca7c21024d682201d4a4cb
Pedro.Marçal <pedro.h.silva.marcal@gmail.com>
2020-08-15 12:24:55 -0300

    Update exercicios_para_modulo.js

--- programacao_computadores/exercicios_para_modulo.js
+++ programacao_computadores/exercicios_para_modulo.js
@@ -18,22 +18,22 @@ principal()
 /*=-=-=-=-=-=-=-=-=--=-=- exe2 -=-=-=-=--=-=-=-=--=-=-=-=-*/
 function exe2 (){
     entrada2()
-    calculo2() 
+    calculo2(s, m, h) 
     console.log(`Em segundos seu valor fica ${resultado}`)
 }
 
 function entrada2 (){
     console.log(`Por favor entre com 3 números Inteiros`)
-    var h = Number(prompt(`Insira o primeiro valor Inteiro`))
-    var m = Number(prompt(`Insira o segundo número`))
-    var s = Number(prompt(`Insira o terceiro valor`))
+    h = Number(prompt(`Insira o primeiro valor Inteiro`))
+    m = Number(prompt(`Insira o segundo número`))
+    s = Number(prompt(`Insira o terceiro valor`))
      
     return h, m, s
 }
 
-function calculo2 (){
-    var m = (h * 60) + m /* horas para minutos + minutos */
-    var resultado = (m * 60) + s /* minutos para segundos */ 
+function calculo2 (h, m, s){
+    m = (h * 60) + m /* horas para minutos + minutos */
+    resultado = (m * 60) + s /* minutos para segundos */ 
 
     return resultado
 }
