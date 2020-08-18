2d89f65b1964e321e072cae497b326ec8517086f
Pedro.Marçal <pedro.h.silva.marcal@gmail.com>
2020-08-17 22:19:05 -0300

    Update aula_dia_17-08.js

--- programacao_computadores/aula_dia_17-08.js
+++ programacao_computadores/aula_dia_17-08.js
@@ -6,10 +6,24 @@ function cadastra(vet){
     objeto.nome = String(prompt(`Indique o nome`))
     vet.push(objeto)
 }
+function mostra(vet){
+    for (var i = 0; i < vet.length; I++){
+        console.log(vet[i])
+    }
+}
 function chefe (){
     var vetor = []
-    cadastra(vetor)
-    cadastra(vetor)
-    console.log(vetor)    
-}
+     do {
+         var opcao = Number(prompt(`Digite uma opção \n [1]. Cadastra \n [2]. Consulta \n [3]. Sair`))
+         switch(opcao){
+            case 1: cadastra(vetor)
+                break
+            case 2: console.log(vetor) 
+                break
+            case 3: console.log(`Até logo`)
+            default: console.log(`Opção Inválida`)
+         }
+     }
+     while(opcao != 3)
+    }
 chefe()
