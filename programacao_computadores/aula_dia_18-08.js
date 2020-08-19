b32ac09da67cb3063ba725f356a3d2a0bd55488d
Pedro.Marçal <pedro.h.silva.marcal@gmail.com>
2020-08-18 21:35:11 -0300

    Update aula_dia_18-08.js

--- programacao_computadores/aula_dia_18-08.js
+++ programacao_computadores/aula_dia_18-08.js
@@ -1,3 +1,36 @@
+exe13()
+// \n quebra a linha no template string
+
 function exe13(){
-    
+    //vetor com escopo global
+    var vetor = []
+    do {
+    var opcao = Number(prompt(`Escolha \n [1].cadastrar \n [2] média \n [3] menor e maior idade \n [4] quantidade de mulheres \n [5] Sair do problema`))
+    switch (opcao){
+        case 1: entrada13(vetor)
+            break
+        case 2: mediaSalario(vetor)
+            break
+        case 3: Idades(vetor)
+            break
+        case 4: qtndMulheres(vetor)
+            break
+        case 5: console.log(`Obrigado por utilizar nosso programa`)
+        default:  console.log(`Opção Inválida`)
+    }
+    entrada13(vetor)
+    mediaSalario(vetor)
+    Idades(vetor)
+    qtndMulheres(vetor)
+    } while (opcao != 5)
 }
+function entrada13(vet){
+    for (var i = Number(0); i <= 15; i++){
+        var vet = new Object
+        vet.idade = Number(prompt(`Insira sua idade`))
+        vet.sexo = String(prompt(`Insira o Sexo`))
+        vet.salario = Number(prompt(`Insira o sálario`))
+        vet.filhos = Number(prompt(`Insira o número de filhos`))
+        vetor.push(vet)
+    }
+}   
