d93911b0eb0c9cba41a534bcd812478ec8167720
Pedro.Marçal <pedro.h.silva.marcal@gmail.com>
2020-08-21 15:32:40 -0300

    aula dia 20

--- programacao_computadores/aula_dia_18-08.js
+++ programacao_computadores/aula_dia_18-08.js
@@ -27,28 +27,46 @@ function exe13(){
 }
 function entrada13(vet){
     console.log(`Entrada de dados`)
-        var vet = new Object
-        vet.idade = Number(prompt(`Insira sua idade`))
-        vet.sexo = String(prompt(`Insira o Sexo`))
-        vet.salario = Number(prompt(`Insira o sálario`))
-        vet.filhos = Number(prompt(`Insira o número de filhos`))
-        vetor.push(vet)
+        var obj = new Object
+        obj.idade = Number(prompt(`Insira sua idade`))
+        obj.sexo = prompt(`Insira o Sexo M/F`).toUpperCase
+        obj.salario = Number(prompt(`Insira o sálario`))
+        obj.filhos = Number(prompt(`Insira o número de filhos`))
+        
+        vet.push(obj)
 }   
+
 function mediaSalario(vet){
     var somasalario
     console.log(`Media salarial`)
-    if (vetor.length != 0){
-        for (var i = Number(0); i = vetor.length; i++){
+    if (vet.length != 0){
+        for (var i = Number(0); i < vetor.length; i++){
             somasalario = vetor[i].salario + somasalario 
         }
     } else {console.log(`Cadastre um valor`)}
 
-    return somasalario / vetor.length
-}
-function Idades (vet){
-    console.log(`Entrou na função menor maior idade`)
+    console.log(`A media salarial ${somasalario / vet.length}`)
 
+function Idades (vet){
+    var maiorIdade = vet[0].idade
+    var menorIdade = vet[0].idade
+    console.log(`Entrou na função menor maior idade`)
+    if (vet == []){
+        console.log(`Cadastre um habitante`)
+        } else {
+            for (var i = Number(0); i < vet.length; i++){
+                if (vet[i].idade > maiorIdade){
+                    maiorIdade = vet[i].idade
+                }
+                if (vet[i].idade < menorIdade){
+                    menorIdade = vet[i].idade
+                }
+            }  
+            console.log(`Maior: ${maiorIdade}, Menor: ${menorIdade}`)
+        }
+    }
 }
+
 function qtndMulheres (vet) {
     console.log(`Quantidade de mulheres`)
 }
