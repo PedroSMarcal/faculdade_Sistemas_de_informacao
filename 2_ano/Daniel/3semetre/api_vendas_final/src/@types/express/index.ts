297c1d27dc3b05f4b94868303a8bfc74298c4d51
Pedro Hnerique Silva <pedro.h.silva.marcal@gmail.com>
2022-02-21 09:13:42 -0300

    Cleaning and add the Readme

--- 2_ano/Daniel/3semetre/api_vendas_final/src/@types/express/index.ts
+++
@@ -1,7 +0,0 @@
-declare namespace Express{
-    export interface Request {
-        user: { // adicionar a variaveis user a campo id
-            id: string;
-        }
-    }
-}
--- 2_ano/Daniel/3semetre/api_vendas_final/src/shared/typeorm/index.ts
+++
@@ -1,5 +0,0 @@
-
-// importa uma função chamada createConnection
-import { createConnection } from "typeorm";
-
-createConnection(); // cria conexão com o banco
